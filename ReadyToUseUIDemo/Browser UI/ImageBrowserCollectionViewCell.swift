bf84e7cf4238b0690db3a7def34789fccbfdd844
Sebastian Husche <sebastian@scanbot.io>
2018-06-06 18:36:36 +0200

    Add new project for demoing ReadyToUse UI

---
+++ ReadyToUseUIDemo/Browser UI/ImageBrowserCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageBrowserCollectionViewCell.swift
+//  ReadyToUseUIDemo
+//
+//  Created by Sebastian Husche on 06.06.18.
+//  Copyright © 2018 doo GmbH. All rights reserved.
+//
+
+import UIKit
+
+class ImageBrowserCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+}
