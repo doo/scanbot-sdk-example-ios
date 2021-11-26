b4056c1a434b3668e451f4c63f0ab359dc05bedf
Daniil Voitenko <daniil@scanbot.io>
2021-11-26 16:37:54 +0100

    convert selection demo screen

---
+++ ScanbotSDKDemo/DemoSelectionTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DemoSelectionTableViewController.swift
+//  ScanbotSDKDemo
+//
+//  Created by Danil Voitenko on 26.11.21.
+//  Copyright © 2021 doo GmbH. All rights reserved.
+//
+
+import UIKit
+
+class DemoSelectionTableViewController: UITableViewController {
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+}
