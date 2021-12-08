3c9489bb9a4013ccf897ee1c872421e3ab13f292
Eduard Frank <eddy@scanbot.io>
2021-12-08 15:55:40 +0100

    add license key notes

--- ScanbotSDKDemo/AppDelegate.swift
+++ ScanbotSDKDemo/AppDelegate.swift
@@ -18,6 +18,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         ScanbotSDK.setupDefaultLicenseFailureHandler()
         
+        // TODO Add your Scanbot SDK license here.
+        // Please note: The Scanbot SDK will run without a license key for one minute per session!
+        // After the trial period has expired all Scanbot SDK functions as well as the UI components will stop working.
+        // You can get an unrestricted "no-strings-attached" trial license key for free.
+        // Please submit the trial license form (https://scanbot.io/en/sdk/demo/trial) on our website by using
+        // the Bundle Identifier "io.scanbot.example.sdk.ios.classic" of this example app.
+
+        //ScanbotSDK.setLicense("YOUR_LICENSE_KEY")
+        
         return true
     }
     
