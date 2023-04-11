05de8a34bc5ce9435f89e310f6259c8e5e0de4ce
ranascanbot <117281220+ranascanbot@users.noreply.github.com>
2023-04-11 16:34:59 +0200

    review changes

--- ScanbotSDKDemo/Data Detectors/BarcodeScannerDemo/Barcode Stable Image Counter/BarcodeStableImageCounterViewController.swift
+++ ScanbotSDKDemo/Data Detectors/BarcodeScannerDemo/Barcode Stable Image Counter/BarcodeStableImageCounterViewController.swift
@@ -12,14 +12,14 @@ import ScanbotSDK
 class BarcodeStableImageCounterViewController: UIViewController {
     
     @IBOutlet private var cameraView: UIView!
-    @IBOutlet private var listTableView: UITableView!
+    @IBOutlet private var tableView: UITableView!
     
     private var scannerViewController: SBSDKBarcodeScannerViewController?
     private var barcodeScanner: SBSDKBarcodeScanner?
     private var barcodeScannerTypes = SBSDKBarcodeType.allTypes()
     private var barcodeResults: [SBSDKBarcodeScannerResult]?
     private var barcodePolygonShapeLayers = [CAShapeLayer]()
-    private var showingResult = false
+    private var isShowingResult = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,38 +32,48 @@ class BarcodeStableImageCounterViewController: UIViewController {
         barcodeScanner = SBSDKBarcodeScanner(types: barcodeScannerTypes)
     }
     
-    @IBAction func captureCountBtn(sender: UIButton) {
-        guard !showingResult else { return }
-        scannerViewController?.captureJPEGStillImage(completionHandler: { image, _ in
-            if let image {
-                self.showingResult = true
-                
-                // freeze the camera
-                self.scannerViewController?.freezeCamera()
-                
-                // get results from the detector
-                self.barcodeResults = self.barcodeScanner?.detectBarCodes(on: image)
-                
-                // draw polygons
-                self.drawPolygonsFrom(image: image)
-                
-                // populate barcode results on the list
-                DispatchQueue.main.async {
-                    self.listTableView.reloadData()
-                }
-                
-                // unfreeze camera after 3 seconds and refresh scanner
-                self.refresh()
+    @IBAction func captureButtonTapped(sender: UIButton) {
+        guard !isShowingResult else { return }
+        isShowingResult = true
+        print(isShowingResult)
+        scannerViewController?.captureJPEGStillImage(completionHandler: { [weak self] image, _ in
+            
+            guard let image else {
+                self?.refreshState()
+                return
             }
+            
+            // freeze the camera
+            self?.scannerViewController?.freezeCamera()
+            
+            // get results from the detector
+            self?.barcodeResults = self?.barcodeScanner?.detectBarCodes(on: image)
+            
+            guard let results = self?.barcodeResults, !results.isEmpty else {
+                self?.refreshState()
+                return
+            }
+            
+            // draw polygons present on the image, onto to the camera view
+            self?.drawPolygons(for: self?.barcodeResults, on: image)
+            
+            // populate barcode results on the list
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+            
+            // refresh after 3 seconds
+            self?.refreshState(withDelay: 3)
         })
     }
     
-    private func drawPolygonsFrom(image: UIImage) {
+    private func drawPolygons(for barcodes: [SBSDKBarcodeScannerResult]?, on image: UIImage) {
         self.barcodeResults?.forEach({ result in
-            DispatchQueue.main.async {
+            DispatchQueue.main.async { [weak self] in
                 
                 // bezier path of barcode on image
-                guard let bezierPath = result.polygon.bezierPath(for: image.size) else { return }
+                guard let bezierPath = result.polygon.bezierPath(for: image.size),
+                      let self else { return }
                 
                 // transform to camera view coordinate system
                 let transform = CGAffineTransform(scaleX: self.cameraView.bounds.size.width/image.size.width,
@@ -71,7 +81,7 @@ class BarcodeStableImageCounterViewController: UIViewController {
                 bezierPath.apply(transform)
                 
                 // show polygon on camera view
-                let shapeLayer = self.shapeLayerFor(bezierPath: bezierPath)
+                let shapeLayer = self.shapeLayer(for: bezierPath)
                 self.barcodePolygonShapeLayers.append(shapeLayer)
                 self.cameraView.layer.insertSublayer(shapeLayer,
                                                      at: UInt32(self.cameraView.layer.sublayers?.count ?? 0))
@@ -79,23 +89,23 @@ class BarcodeStableImageCounterViewController: UIViewController {
         })
     }
     
-    private func shapeLayerFor(bezierPath path: UIBezierPath) -> CAShapeLayer {
+    private func shapeLayer(for bezierPath: UIBezierPath) -> CAShapeLayer {
         let shapeLayer = CAShapeLayer()
-        shapeLayer.path = path.cgPath
+        shapeLayer.path = bezierPath.cgPath
         shapeLayer.strokeColor = UIColor.cyan.cgColor
         shapeLayer.fillColor = UIColor.clear.cgColor
         shapeLayer.lineWidth = 2.0
         return shapeLayer
     }
     
-    private func refresh() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+    private func refreshState(withDelay delay: Int = 0) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
             self.barcodeResults?.removeAll()
             self.barcodePolygonShapeLayers.forEach({$0.removeFromSuperlayer()})
             self.barcodePolygonShapeLayers.removeAll()
-            self.listTableView.reloadData()
+            self.tableView.reloadData()
             self.scannerViewController?.unfreezeCamera()
-            self.showingResult = false
+            self.isShowingResult = false
         }
     }
 }
