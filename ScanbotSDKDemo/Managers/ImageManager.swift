1962937f2f54231499dbb6ebeef0eea1e274b5ee
Sebastian Husche <sebastian@scanbot.io>
2024-05-13 11:34:39 +0200

    Fix EPIC-4649

--- ScanbotSDKDemo/Managers/ImageManager.swift
+++ ScanbotSDKDemo/Managers/ImageManager.swift
@@ -10,9 +10,9 @@ import Foundation
 import ScanbotSDK
 
 class ImageProcessingParameters {
-    var polygon: SBSDKPolygon
-    var filter: SBSDKImageFilterType
-    var counterClockwiseRotations: Int
+    var polygon: SBSDKPolygon?
+    var filter: SBSDKImageFilterType?
+    var counterClockwiseRotations: Int?
     
     init() {
         polygon = SBSDKPolygon()
@@ -20,7 +20,7 @@ class ImageProcessingParameters {
         counterClockwiseRotations = 0
     }
     
-    init(polygon: SBSDKPolygon, filter: SBSDKImageFilterType, counterClockwiseRotations: Int) {
+    init(polygon: SBSDKPolygon?, filter: SBSDKImageFilterType?, counterClockwiseRotations: Int?) {
         self.polygon = polygon
         self.filter = filter
         self.counterClockwiseRotations = counterClockwiseRotations
@@ -31,125 +31,61 @@ final class ImageManager {
     
     public static let shared = ImageManager()
     
-    public let processedImageStorage = storageForDirectory("/sbsdk-demo-processed-images")
-    private let originalImageStorage = storageForDirectory("/sbsdk-demo-original-images")
-    private var processingParameters = [URL : ImageProcessingParameters]()
+    public let document = SBSDKDocument()
     
     private init() {
         self.removeAllImages()
     }
     
-    static private func storageForDirectory(_ directory: String) -> SBSDKIndexedImageStorage {
-        let url = SBSDKStorageLocation.applicationDocumentsFolderURL.appendingPathComponent(directory)
-        let location = SBSDKStorageLocation(baseURL: url)
-        guard let storage = SBSDKIndexedImageStorage(storageLocation: location) else {
-            fatalError("Failed to create the storage")
-        }
-        return storage
-    }
-    
     var numberOfImages: Int {
-        return Int(originalImageStorage.imageCount)
+        return document.pages.count 
     }
     
     @discardableResult
     func add(image: UIImage, polygon: SBSDKPolygon) -> Bool {
-        if originalImageStorage.add(image) {
-            let parameters = ImageProcessingParameters(polygon: polygon,
-                                                       filter: .none,
-                                                       counterClockwiseRotations: 0)
-            if processImageAt(Int(originalImageStorage.imageCount-1), withParameters: parameters) {
-                return true
-            } else {
-                originalImageStorage.removeImage(at: originalImageStorage.imageCount - 1)
-            }
-        }
-        return false
+        let page = SBSDKDocumentPage(image: image, polygon: polygon, filter: .none)
+        return document.add(page)
+    }
+    
+    func pageAt(index: Int) -> SBSDKDocumentPage? {
+        return document.page(at: index)
     }
     
     func originalImageAt(index: Int) -> UIImage? {
-        if index < 0 || index >= numberOfImages { return nil }
-        return originalImageStorage.image(at: index)
+        return document.page(at: index)?.originalImage
     }
 
     func originalImageURLAt(index: Int) -> URL? {
-        if index < 0 || index >= numberOfImages { return nil }
-        return originalImageStorage.imageURL(at: index)
+        return document.page(at: index)?.originalImageURL
     }
 
     func processedImageAt(index: Int) -> UIImage? {
-        if index < 0 || index >= numberOfImages { return nil }
-        return processedImageStorage.image(at: index)
+        return document.page(at: index)?.documentImage
     }
-    
-    func processingParametersAt(index: Int) -> ImageProcessingParameters? {
-        guard let url = originalImageStorage.imageURL(at: index) else {
-            return nil
-        }
         
-        if let parameters = processingParameters[url] {
-            return parameters
-        } else {
-            let parameters = ImageProcessingParameters()
-            storeParameters(parameters, at: url)
-            return parameters
-        }
-    }
-    
-    func storeParameters(_ parameters: ImageProcessingParameters, at url: URL) {
-        processingParameters[url] = parameters
-    }
-    
     func removeImageAt(index: Int) {
-        originalImageStorage.removeImage(at: index)
-        processedImageStorage.removeImage(at: index)
-        if let url = originalImageStorage.imageURL(at: index) {
-            processingParameters.removeValue(forKey: url)
-        }
+        document.removePage(at: index)
     }
     
     func removeAllImages() {
-        originalImageStorage.removeAll()
-        processedImageStorage.removeAll()
-        processingParameters.removeAll()
+        document.removeAllPages()
     }
     
     func processImageAt(_ index: Int, withParameters parameters: ImageProcessingParameters) -> Bool {
-        if index < 0 || index >= numberOfImages { return false }
         
-        guard let image = originalImageStorage.image(at: index) else {
+        guard let page = document.page(at: index) else {
             return false
         }
         
-        guard let rotImage = image.sbsdk_imageRotatedCounterClockwise(parameters.counterClockwiseRotations) else {
-            return false
+        if let rotations = parameters.counterClockwiseRotations {
+            page.rotateClockwise(-rotations)
         }
-        
-        let filter = SBSDKLegacyFilter(legacyFilter: parameters.filter)
-        guard let processedImage = rotImage.sbsdk_imageWarped(by: parameters.polygon,
-                                                              filters: [filter],
-                                                              imageScale: 1.0) else {
-            return false
+        if let polygon = parameters.polygon {
+            page.polygon = polygon
         }
-        
-        parameters.counterClockwiseRotations = 0
-        let _ = originalImageStorage.replaceImage(at: index, with: rotImage)
-
-        var success = true
-        if index < processedImageStorage.imageCount {
-            success = processedImageStorage.replaceImage(at: index, with: processedImage)
-        } else {
-            success = processedImageStorage.add(processedImage)
+        if let filter = parameters.filter {
+            page.filter = filter
         }
-        
-        if !success {
-            return false
-        }
-        
-        guard let url = originalImageStorage.imageURL(at: index) else {
-            return false
-        }
-        storeParameters(parameters, at: url)
         return true
     }
 }
