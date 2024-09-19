1095b431346f7bd4cf0f48e47851e4ed4b4d9389
ranascanbot <rana@scanbot.io>
2024-09-19 16:44:02 +0200

    Update examples

---
+++ Documentation Code Snippets/ScanbotSDK Examples/DocumentScanner/RTUUIV2/Crop/CropScreenUI2SwiftViewController.swift
@@ -0,0 +1,47 @@
+//
+//  CropScreenUI2SwiftViewController.swift
+//  ScanbotSDK Examples
+//
+//  Created by Seifeddine Bouzid on 01.08.24.
+//
+
+import Foundation
+
+import Foundation
+import ScanbotSDK
+
+class CropScreenUI2SwiftViewController: UIViewController {
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Create the default configuration object.
+        let configuration = SBSDKUI2DocumentScanningFlow()
+        
+        // Retrieve the instance of the crop configuration from the main configuration object.
+        let cropScreenConfiguration = configuration.screens.cropping
+        
+        // e.g disable the rotation feature.
+        cropScreenConfiguration.bottomBar.rotateButton.visible = false
+        
+        // e.g. configure various colors.
+        configuration.appearance.topBarBackgroundColor = SBSDKUI2Color(uiColor: UIColor.red)
+        cropScreenConfiguration.topBarConfirmButton.foreground.color = SBSDKUI2Color(uiColor: UIColor.white)
+        
+        // e.g. customize a UI element's text
+        configuration.localization.croppingCancelButtonTitle = "Cancel"
+        
+        // Present the recognizer view controller modally on this view controller.
+        SBSDKUI2DocumentScannerController.present(on: self, configuration: configuration) { document in
+            
+            // Completion handler to process the result.
+            
+            if let document {
+                // Handle the document.
+                
+            } else {
+                // Indicates that the cancel button was tapped.
+            }
+        }
+    }
+}
