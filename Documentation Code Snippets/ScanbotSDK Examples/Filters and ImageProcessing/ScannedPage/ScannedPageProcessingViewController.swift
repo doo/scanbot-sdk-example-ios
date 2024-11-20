382eab1704d6b1c734b996764ff3ffc8a1891f08
ranasohaib <ranasohaib002@icloud.com>
2024-11-20 12:52:07 +0100

    update example

--- Documentation Code Snippets/ScanbotSDK Examples/Filters and ImageProcessing/ScannedPage/ScannedPageProcessingViewController.swift
+++ Documentation Code Snippets/ScanbotSDK Examples/Filters and ImageProcessing/ScannedPage/ScannedPageProcessingViewController.swift
@@ -32,8 +32,16 @@ class ScannedPageProcessingViewController: UIViewController {
         // Create the instances of the filters you want to apply.
         let filter1 = SBSDKScanbotBinarizationFilter(outputMode: .antialiased)
         let filter2 = SBSDKBrightnessFilter(brightness: 0.4)
+
+        // Apply individual changes using the properties of the page object.
+        // E.g. rotate the page 90 degrees clockwise.
+        page.rotation = .clockwise90
         
         // Apply the filters.
         page.filters = [filter1, filter2]
+        
+        // If you want to apply multiple changes to the page at once, use the apply(...) function.
+        // This will result in a faster performance than applying each change individually.
+        page.apply(rotation: .clockwise90, polygon: SBSDKPolygon(), filters: [filter1, filter2])
     }
 }
