15b682df854cafd6a9ec493529647565d9ad2e01
ranascanbot <rana@scanbot.io>
2024-12-05 18:43:33 +0100

    Add data parsing view controller example

---
+++ Documentation Code Snippets/ScanbotSDK Examples/Barcode Scanner/Data Parsing/Objc/BarcodeScannerDataParsingObjcViewController.h
@@ -0,0 +1,16 @@
+//
+//  BarcodeScannerDataParsingObjcViewController.h
+//  ScanbotSDK Examples
+//
+//  Created by Danil Voitenko on 02.06.21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BarcodeScannerDataParsingObjcViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
