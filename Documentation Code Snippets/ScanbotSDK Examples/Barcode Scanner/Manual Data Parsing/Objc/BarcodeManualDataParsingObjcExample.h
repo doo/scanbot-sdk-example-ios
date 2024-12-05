713742c62d5f2fbc442d09a0349e2a10fce058ad
ranascanbot <rana@scanbot.io>
2024-12-05 18:12:09 +0100

    add manual barcode document parsing doc code snippet

---
+++ Documentation Code Snippets/ScanbotSDK Examples/Barcode Scanner/Manual Data Parsing/Objc/BarcodeManualDataParsingObjcExample.h
@@ -0,0 +1,16 @@
+//
+//  BarcodeManualDataParsingObjcExample.h
+//  ScanbotSDK Examples
+//
+//  Created by Rana Sohaib on 05.12.24.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BarcodeManualDataParsingObjcExample : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
