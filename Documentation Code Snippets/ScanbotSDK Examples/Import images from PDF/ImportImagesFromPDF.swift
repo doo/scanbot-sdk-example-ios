b86a35802ed55305ba80e551b6df4e8f1f1ce8c5
scanbot-ci <ci@scanbot.io>
2025-06-27 16:31:41 +0200

    Copied Full SDK code snippets for release v7.1.0.

--- Documentation Code Snippets/ScanbotSDK Examples/Import images from PDF/ImportImagesFromPDF.swift
+++ Documentation Code Snippets/ScanbotSDK Examples/Import images from PDF/ImportImagesFromPDF.swift
@@ -22,10 +22,11 @@ func createImagesFromPDF(pdfURL: URL) {
     
     // Create an instance of the PDF page extractor.
     let pageExtractor = SBSDKPDFImageExtractor()
+    pageExtractor.scaleFactor = 2.0 // Optional: Set scaling for the extracted images.
     
     // Extract the pages from the PDF and returns an array of UIImage
     let images = pageExtractor.extract(from: pdfURL)
     
     // You can also use `extract(from:scaling:)` method to extract images with a scaling applied.
-    let scaledImages = pageExtractor.extract(from: pdfURL, scaling: 2.0)
+    let scaledImages = pageExtractor.extract(from: pdfURL)
 }
