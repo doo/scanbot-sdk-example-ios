d503616c73f4f74d3a69c30e10f7df938468c293
ranasohaib <ranasohaib002@icloud.com>
2025-07-16 14:26:06 +0200

    direct document detection example

---
+++ Documentation Code Snippets/ScanbotSDK Examples/DocumentScanner/Scan on image/Direct scanned document detection/DirectScannedDocumentDetectionViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DirectScannedDocumentDetectionViewController.swift
+//  ScanbotSDK Examples
+//
+//  Created by Rana Sohaib on 16.07.25.
+//
+
+import Foundation
+import ScanbotSDK
+
+class DirectScannedDocumentDetectionViewController {
+    
+    func detectDocument(on scannedDocument: SBSDKScannedDocument) {
+        
+        // Iterate over the pages.
+        scannedDocument.pages.forEach { page in
+            
+            // Create an instance of a document scanner.
+            let scanner = SBSDKDocumentScanner()
+            
+            // Retrieve scanned document's original image.
+            guard let image = page.originalImage else { return }
+            
+            // Run detection on the image.
+            guard let result = scanner.scan(from: image) else { return }
+            
+            // Check the result and retrieve the detected polygon.
+            if result.status == .ok, let polygon = result.polygon {
+                
+                // Apply the polygon on to the page.
+                page.apply(polygon: polygon)
+                
+            } else {
+                
+                // No acceptable polygon found.
+            }
+        }
+    }
+}
